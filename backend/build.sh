d7db00ac78a27a6f67430a9b81e92e73bff27ae4
NGnius (Graham) <ngniusness@gmail.com>
2024-03-17 17:35:07 -0400

    version 2.0.0-beta1

--- backend/build.sh
+++ backend/build.sh
@@ -3,10 +3,11 @@
 #cargo build --release --target x86_64-unknown-linux-musl
 #cargo build --target x86_64-unknown-linux-musl
 #cross build
-cargo build
+cargo build --release
+#cargo build
 
 mkdir -p ../bin
 #cp --preserve=mode ./target/x86_64-unknown-linux-musl/release/powertools ../bin/backend
 #cp --preserve=mode ./target/x86_64-unknown-linux-musl/debug/powertools ../bin/backend
+cp --preserve=mode ./target/release/powertools ../bin/backend
 #cp --preserve=mode ./target/debug/powertools ../bin/backend
-cp --preserve=mode ./target/debug/powertools ../bin/backend
